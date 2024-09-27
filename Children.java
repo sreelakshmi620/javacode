7df03068da43d70ea760c68cb984776e56dad4ad
sree lakshmi <srilakshmi804@.com>
2024-09-27 08:06:44 +0530

    assignments on basic java

---
+++ Children.java
@@ -0,0 +1,16 @@
+package Access2;
+
+import Access3.Professor;
+public class Children extends Professor/// this is only for accessioing  public& protected
+{
+
+	public static void main(String[]args) 
+	{
+		Children c1=new Children();
+		c1.name();
+		c1.marks();
+		
+		
+		
+	}
+}
